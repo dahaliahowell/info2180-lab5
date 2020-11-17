57146ff46bee5c7433abab234d68b5e962ea6e72
Dahalia Howell <dahaliahowell@Dahalias-MacBook-Pro.local>
2020-11-17 12:37:17 -0500

    Added feature to display cities in a given country

--- world.js
+++ world.js
@@ -1,6 +1,7 @@
 window.onload = function() {
 
     var searchBtn = document.getElementById("lookup");
+    var searchBtnCities = document.getElementById("lookup-cities");
     var httpRequest;
   
     searchBtn.addEventListener('click', function(element) {
@@ -15,6 +16,19 @@ window.onload = function() {
       httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
       httpRequest.send('name=' + encodeURIComponent(country));
     });
+
+    searchBtnCities.addEventListener('click', function(element) {
+        element.preventDefault();
+    
+        httpRequest = new XMLHttpRequest();
+        var country = document.querySelector('#country').value;
+    
+        var url = "http://localhost:8888/info2180-lab5/world.php?country=" + country + "&context=cities";
+        httpRequest.onreadystatechange = sendResult;
+        httpRequest.open('GET', url);
+        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        httpRequest.send('name=' + encodeURIComponent(country));
+      });
   
     function sendResult() {
       if (httpRequest.readyState === XMLHttpRequest.DONE) {
--- world.php
+++ world.php
@@ -6,17 +6,50 @@ $dbname = 'world';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
-  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $check = False;
   $country = $_GET['country'];
-  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
-  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  
+  if ($_GET['context'] === 'cities') {
+
+    $check = True;
+    $stmt = $conn->query("SELECT c.id, c.name, c.country_code, c.district, c.population, co.name as country, co.code FROM cities c JOIN countries co ON c.country_code=co.code WHERE co.name = '$country'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  } else {
+    
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  }
 
 
 }
 
 
 ?>
+<?php if ($check === True): ?>
+  <?php if (empty($results)): ?>
+    <h2>NO RESULTS</h2>
+  <?php else: ?>
+  <table>
+  <tr>
+    <th>Name</th>
+    <th>District</th>
+    <th>Population</th>
+  </tr>
+<?php foreach ($results as $row): ?>
+  <tr>
+    <td><?= $row['name']; ?></td>
+    <td><?= $row['district']; ?></td>
+    <td><?= $row['population']; ?></td>
+  </tr>
+<?php endforeach; ?>
+</table>
+<?php endif; ?>
+<?php else: ?>
 <table>
   <tr>
     <th>Name</th>
@@ -33,3 +66,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
   </tr>
 <?php endforeach; ?>
 </table>
+<?php endif; ?>
