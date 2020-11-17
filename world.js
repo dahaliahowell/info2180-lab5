faaa909c36ca28e0db8a4556e2d007b37830441f
Dahalia Howell <dahaliahowell@Dahalias-MacBook-Pro.local>
2020-11-17 02:19:46 -0500

    Added lookup country feature

---
+++ world.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+
+    var searchBtn = document.getElementById("lookup");
+    var httpRequest;
+  
+    searchBtn.addEventListener('click', function(element) {
+      element.preventDefault();
+  
+      httpRequest = new XMLHttpRequest();
+      var country = document.querySelector('#country').value;
+  
+      var url = "http://localhost:8888/info2180-lab5/world.php?country=" + country;
+      httpRequest.onreadystatechange = sendResult;
+      httpRequest.open('GET', url);
+      httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      httpRequest.send('name=' + encodeURIComponent(country));
+    });
+  
+    function sendResult() {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          var response = httpRequest.responseText;
+          document.getElementById("result").innerHTML = response;
+        } else {
+          alert('There was a problem with the request.');
+        }
+      }
+    }
+  
+};
